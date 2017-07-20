083b7d2e4ceeefe1301097774225d369ab5dc673
Wagner Macedo <wagnerluis1982@gmail.com>
2017-07-20 19:42:37 -0300

    Update assets/dispatcher.js
    Muda de onload -> iife

--- assets/dispatcher.js
+++ assets/dispatcher.js
@@ -1,8 +1,8 @@
 /**
  * Created by wagner on 18/07/17.
  */
-window.onload = function () {
+(function () {
     document.querySelector('body').innerText = 'Carregando...';
     var q = window.location.pathname.split(/[\\/]/).pop().replace(/.html$/, '');
     window.location = 'index.html?aula=' + q;
-};
+})();

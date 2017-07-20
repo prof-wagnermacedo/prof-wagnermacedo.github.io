beb8ec45ed9a7404cf02a8439993df475e40412b
Wagner Macedo <wagnerluis1982@gmail.com>
2017-07-20 00:35:43 -0300

    Removido uso de document.write

--- assets/dispatcher.js
+++ assets/dispatcher.js
@@ -1,13 +1,8 @@
 /**
  * Created by wagner on 18/07/17.
  */
-$(function () {
-    aula = $('#aula').html();
-
-    $.get('index.html',
-        function (response) {
-            document.open('text/html', 'replace');
-            document.write(response);
-            document.close();
-        });
-});
+window.onload = function () {
+    document.querySelector('body').innerText = 'Carregando...';
+    var q = window.location.pathname.split(/[\\/]/).pop().replace(/.html$/, '');
+    window.location = 'index.html?aula=' + q;
+};

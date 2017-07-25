1047c864f4bcaa51ecd313001feb3cf2dc7a0f5d
Wagner Macedo <wagnerluis1982@gmail.com>
2017-07-25 19:00:22 -0300

    Atualização na estrutura html-demo

--- assets/professor-utils.js
+++ assets/professor-utils.js
@@ -4,6 +4,13 @@
 
 /* Carrega a apresentação ou mostra índice */
 (function () {
+    var scripts = document.getElementsByTagName('script');
+    var scriptPath = scripts[scripts.length - 1].src.replace(/\/[^/]+$/, '');
+
+    var isPrinting = function () {
+        return window.location.search.match(/print-pdf/gi);
+    };
+
     var displaySummary = function () {
         $.get('Z', function (response) {
             var summary = $('#slides').find('div');
@@ -38,66 +45,95 @@
     } else {
         displaySummary();
     }
-})();
 
-/* Helper para demonstrar HTML */
-function postLoadAula() {
-    $('.revisao').each(function () {
-        var $this = $(this);
-        var $demo = $this.find('.demo');
-        var $codigo = $this.find('.codigo');
+    /* Helper para demonstrar HTML */
+    function postLoadAula() {
+        $('.revisao').each(function () {
+            var $this = $(this);
+            var $demo = $this.find('.demo');
+            var $codigo = $this.find('.codigo');
 
-        //region Coloca o HTML em $demo
-        var html;
+            //region Coloca o HTML em $demo
+            var codigoFonte = $codigo.html();
+            var codigoHtml = codigoFonte;
 
-        if ($demo.hasClass('par')) {
-            html = $codigo.html().trim().split(/[\r\n]+/)
-                .map(function (item) {
-                    return $('<p></p>').html(item);
-                });
-        } else {
-            html = $codigo.html();
-        }
+            if ($codigo[0].hasAttribute('data-noescape')) {
+                var doc = new DOMParser().parseFromString(codigoHtml, 'text/html');
+                codigoHtml = doc.documentElement.textContent;
+            }
 
-        if ($demo.is('iframe')) {
-            $demo.contents().find('body')
-                .append(html,
-                    '<style>' +
-                    'body { margin: 5px; font-size: 1.9em; }' +
-                    'body * { margin: 0; }' +
-                    '</style>');
-        }
-        else {
-            $demo.html(html);
-        }
-        //endregion
+            if ($demo.hasClass('par')) {
+                codigoHtml = codigoFonte.trim().split(/[\r\n]+/)
+                    .map(function (item) {
+                        return $('<div></div>').html(item);
+                    });
+            }
 
-        // Código entre <pre><code>
-        $codigo.wrapInner('<pre><code class="html" data-trim></code></pre>');
-    });
-}
+            if ($demo.is('iframe')) {
+                $demo.attr('src', scriptPath + '/show-demo.html')
+                    .on('load', function () {
+                        var demoContents = $demo.contents();
+                        demoContents.find('body').html(codigoHtml);
+                        if (isPrinting()) {
+                            $demo.height(demoContents.height());
+                        }
+                    });
+            } else {
+                $demo.html(codigoHtml);
+            }
+            //endregion
 
-// Redimensiona todos os iframe.demo corretamente
-Reveal.addEventListener('ready', function (readyEvent) {
-    // Para o slide inicializado aqui
-    $(readyEvent.currentSlide)
-        .find('iframe.demo')
-        .each(function () {
-            var $iframe = $(this);
-            this.contentWindow.addEventListener('resize', function () {
-                $iframe.height($iframe.contents().height());
+            // Código entre <pre><code>
+            var $code = $('<code class="html" data-trim></code>');
+
+            // Escapa código, a não ser quando houver 'noescape'
+            if (!$codigo[0].hasAttribute('data-noescape')) {
+                codigoFonte = codigoFonte.replace(/&(?=(?:[a-zA-Z]+|#[0-9]+);)/g, '&amp;');
+            }
+
+            // Copia todos os atributos data-* para a tag code
+            $.each($codigo[0].attributes, function () {
+                if (this.name.match(/^data-/)) {
+                    $code.attr(this.name, '');
+                }
             });
-        });
 
-    // Para todos os outros slides
-    Reveal.addEventListener('slidechanged', function (event) {
-        if (readyEvent.currentSlide !== event.currentSlide) {
-            $(event.currentSlide)
-                .find('iframe.demo')
-                .each(function () {
-                    var $iframe = $(this);
+            $codigo.html($code.html(codigoFonte)
+                .wrap('<pre></pre>')
+                .parent());
+        });
+    }
+
+    // Ações para quando os slides estiverem carregados
+    Reveal.addEventListener('ready', function (readyEvent) {
+        // Abre todos os details, se isPrinting
+        if (isPrinting()) {
+            [].forEach.call(document.getElementsByTagName('details'),
+                function (e) {
+                    e.open = true;
+                });
+        }
+
+        // Redimensiona todos os iframe.demo corretamente
+        $(readyEvent.currentSlide)
+            .find('iframe.demo')
+            .each(function () {
+                var $iframe = $(this);
+                $iframe.on('load', function () {
                     $iframe.height($iframe.contents().height());
                 });
-        }
+            });
+
+        // Redimensiona todos os iframe.demo corretamente
+        Reveal.addEventListener('slidechanged', function (event) {
+            if (readyEvent.currentSlide !== event.currentSlide) {
+                $(event.currentSlide)
+                    .find('iframe.demo')
+                    .each(function () {
+                        var $iframe = $(this);
+                        $iframe.height($iframe.contents().height());
+                    });
+            }
+        });
     });
-});
+})();

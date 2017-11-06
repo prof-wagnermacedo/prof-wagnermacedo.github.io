203edb4243987131a213e8b85cab66df52e13037
Wagner Macedo <wagnerluis1982@gmail.com>
2017-11-06 15:20:29 -0300

    2ª Avaliação

---
+++ 2017-2/pjw/un2/Tarefa.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Tarefa {
+    private Integer id;
+    private String descricao;
+    private boolean concluida;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}

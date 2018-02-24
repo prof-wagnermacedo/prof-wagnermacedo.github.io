de5352f04d4dbc4eeab0aa9fa72bc474a270c4fc
Wagner Macedo <wagnerluis1982@gmail.com>
2018-02-24 02:58:20 -0300

    Adiciona destaque de linhas com lining.js

--- assets/course.js
+++ assets/course.js
@@ -2,7 +2,34 @@ $(function () {
     twemoji.parse(document, {
         folder: 'svg',
         ext: '.svg'
-    })
+    });
+
+    const removeLinebreaks = (_, oldHtml) => oldHtml.replace(/\n/g, '');
+
+    $('[data-hi]').each(function () {
+        const dataHi = this.getAttribute('data-hi');
+
+        $(this).find('pre.highlight > code')
+            .on('afterlining', function () {
+                this.removeAttribute('data-lining');
+                this.innerHTML = this.innerHTML.replace(/[\r\n]/g, '');
+
+                dataHi.split(',').forEach(spec => {
+                    let [lower, upper] = spec.split('-', 2).map(e => parseInt(e));
+
+                    if (upper === undefined) {
+                        upper = lower;
+                    }
+
+                    for (let i = lower; i <= upper; i++) {
+                        $(this).find(`.line[index="${i}"]`).addClass('hi-line');
+                    }
+                });
+            })
+            .each(function () {
+                lining(this);
+            });
+    });
 });
 
 function doCourse() {
